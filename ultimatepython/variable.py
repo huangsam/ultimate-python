85902420ee27137084461538015325e3fa47d31f
Samuel Huang <samhuang91@gmail.com>
2020-08-16 16:13:41 -0700

    Create some initial content

---
+++ ultimatepython/variable.py
@@ -0,0 +1,15 @@
+def main():
+    # There are a couple different literal types
+    a = 1
+    b = 2.0
+    c = True
+    d = "hello"
+
+    print(a, type(a))  # <class 'int'>
+    print(b, type(b))  # <class 'float'>
+    print(c, type(c))  # <class 'bool'>
+    print(d, type(d))  # <class 'str'>
+
+
+if __name__ == "__main__":
+    main()
