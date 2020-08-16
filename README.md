56c70dc33746c415dc4e5154fb6a4f4264249f29
Samuel Huang <samhuang91@gmail.com>
2020-08-16 16:13:41 -0700

    Create some initial content

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ultimate Python study guide
 
 Ultimate Python study guide, influenced by [ultimate-go](https://github.com/hoanhan101/ultimate-go)
+
+- **Language Mechanics**
+    - Variable: [Built-in types](ultimatepython/variable.py)
+    - Expression: [Numeric operations](ultimatepython/expression.py)
---
+++ ultimatepython/expression.py
@@ -0,0 +1,19 @@
+def main():
+    # This is a simple integer
+    x = 1
+
+    # Its value can used as part of expressions
+    print(x + 1)
+    print(x * 2 * 2)
+
+    # Divison is a bit tricky in Python because it returns a result
+    # of type 'float' by default
+    print(x / 2)
+
+    # If you want to compute an integer division, then add
+    # an extra slash to the expression
+    print(x // 2)
+
+
+if __name__ == "__main__":
+    main()
