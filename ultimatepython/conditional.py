118f27aa999a5c3cecceafee89f5f70ddaac2e7e
Samuel Huang <samhuang91@gmail.com>
2020-08-16 19:36:58 -0700

    Make conditional more readable

--- ultimatepython/conditional.py
+++ ultimatepython/conditional.py
@@ -2,30 +2,28 @@ def main():
     x = 1
     x_add_two = x + 2
 
-    # Test that the obvious is true
+    # This condition is obviously true
     if x_add_two == 3:
         print("math wins")  # ran
 
-    # Test that a double-negative is true. This
-    # could also be written as the following:
-    # ... if x_add_two != 1
+    # A negated condition can also be true
     if not x_add_two == 1:
         print("math wins here too")  # ran
 
     # There are else statements as well, which get run
     # if the initial condition fails
     if x_add_two == 1:
-        print("math lost here...")  # skipped
+        print("math lost here...")
     else:
         print("math wins otherwise")  # ran
 
     # ...or gets run after multiple conditions fail
     if x_add_two == 1:
-        print("nope not this one...")  # skipped
+        print("nope not this one...")
     elif x_add_two == 2:
-        print("nope not this one either...")  # skipped
-    elif x_add_two < 3:
-        print("nope not quite...")  # skipped
+        print("nope not this one either...")
+    elif x_add_two < 3 or x_add_two > 3:
+        print("nope not quite...")
     else:
         print("math wins finally")  # ran
 
