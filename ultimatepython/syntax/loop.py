aa992a8c118fcc4faa1a4fcfd46c96f07bebe27a
Samuel Huang <samhuang91@gmail.com>
2020-08-18 15:21:11 -0700

    Update loop.py

--- ultimatepython/syntax/loop.py
+++ ultimatepython/syntax/loop.py
@@ -1,6 +1,6 @@
 def main():
-    # This is a for loop that iterates on values 0..4
-    # and adds each value to total
+    # This is a `for` loop that iterates on values 0..4
+    # and adds each value to `total`
     total = 0
     for i in range(5):
         total += i
@@ -8,8 +8,8 @@ def main():
     # The answer is...10!
     print(f"Sum(0..4) = {total}")
 
-    # This is a for loop that iterates on values 5..1
-    # and multiplies each value to fib
+    # This is a `for` loop that iterates on values 5..1
+    # and multiplies each value to `fib`
     fib = 1
     for i in range(5, 0, -1):
         fib *= i
@@ -17,7 +17,7 @@ def main():
     # The answer is...120!
     print(f"Fibonacci(5..1) = {fib}")
 
-    # This is a simple while loop, similar to a for loop except that the
+    # This is a simple `while` loop, similar to a `for` loop except that the
     # counter is declared outside of the loop and its state is explicitly
     # managed inside of the loop. The loop will continue until the counter
     # exceeds 5
@@ -26,9 +26,9 @@ def main():
         print(f"While {i} < 5")
         i += 2
 
-    # This is a while loop that is stopped with break and its counter is
+    # This is a `while` loop that is stopped with `break` and its counter is
     # multiplied in the loop, showing that you can do anything to the
-    # counter. Like the previous while loop, this one continues until
+    # counter. Like the previous `while` loop, this one continues until
     # the counter exceeds 5
     i = 1
     while True:
@@ -40,16 +40,16 @@ def main():
         if i >= 5:
             print(f"Break out! {i} is no longer < 5")
 
-            # The break statement stops the current while loop.
-            # If this while loop was nested in another loop,
+            # The `break` statement stops the current while loop.
+            # If this `while` loop was nested in another loop,
             # this statement would not stop the parent loop
             break
         else:
-            # The continue statement returns to the
+            # The `continue` statement returns to the
             # start of the current while loop
             continue
 
-        # Skipped because of continue and break
+        # Skipped because of `continue` and `break`
         # print("I will never get called")
 
 
