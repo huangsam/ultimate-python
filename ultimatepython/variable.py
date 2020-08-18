06199f91b4682b82ef21925358e9eb4d1f107bfe
Samuel Huang <samhuang91@gmail.com>
2020-08-18 07:50:08 -0700

    Add more content to variable

--- ultimatepython/variable.py
+++ ultimatepython/variable.py
@@ -6,13 +6,18 @@ def main():
     d = "hello"
 
     # Notice that each type is a 'class'. Each variable
-    # refers to an 'instance' of the class it belongs
-    # to. This leads to an important point: everything
+    # refers to an 'instance' of the class it belongs to
+    a_type = type(a)  # <class 'int'>
+    b_type = type(b)  # <class 'float'>
+    c_type = type(c)  # <class 'bool'>
+    d_type = type(d)  # <class 'str'>
+
+    # This leads to an important point: everything
     # is an object in Python
-    print(a, type(a))  # <class 'int'>
-    print(b, type(b))  # <class 'float'>
-    print(c, type(c))  # <class 'bool'>
-    print(d, type(d))  # <class 'str'>
+    print(isinstance(a, object) and issubclass(a_type, object))
+    print(isinstance(b, object) and issubclass(b_type, object))
+    print(isinstance(c, object) and issubclass(c_type, object))
+    print(isinstance(d, object) and issubclass(d_type, object))
 
 
 if __name__ == "__main__":
