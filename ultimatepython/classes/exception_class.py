1bf9d387594f4dfe298e17b0db12e9309b4ac5e0
Samuel Huang <samhuang91@gmail.com>
2020-08-22 20:49:21 -0700

    Revise exception_class structure

--- ultimatepython/classes/exception_class.py
+++ ultimatepython/classes/exception_class.py
@@ -1,39 +1,39 @@
 class UltimatePythonError(Exception):
-    """Base class of errors for ultimate-python package."""
+    """Base class of errors for ultimate-python package.
+
+    This is the base exception for the Ultimate Python study guide. One of
+    the reasons why developers design a class like this is for consumption
+    by downstream services and command-line tools.
+    """
 
 
 class IterationError(UltimatePythonError, RuntimeError):
     """Any error that comes while iterating through objects."""
 
 
-class BadInputError(UltimatePythonError, ValueError):
-    """Bad input provided by developer."""
-
-    def __init__(self, bad_input, bad_reason):
-        self.bad_input = bad_input
-        self.bad_reason = bad_reason
-
-    def __repr__(self):
-        return f"<BadInputError {self.bad_input} ({type(self.bad_input)})>"
-
-    def __str__(self):
-        return f"Input {self.bad_input} is bad. {self.bad_reason}"
+class DivisionError(UltimatePythonError, ValueError):
+    """Any division error that results from invalid input."""
 
 
-def divide(num_x, num_y):
-    if num_y == 0:
-        raise BadInputError(num_y, "Cannot have a zero for the divisor")
-    return num_x // num_y
+def divide_positive_numbers(dividend, divisor):
+    """Divide a positive number by another positive number"""
+    if divisor == 0:
+        raise DivisionError("Cannot have a zero divisor")
+    elif dividend < 0:
+        raise DivisionError("Cannot have a negative dividend")
+    elif divisor < 0:
+        raise DivisionError("Cannot have a negative divisor")
+    return dividend // divisor
 
 
 def main():
     # Exception classes are no different from concrete classes in that
     # they all have inheritance baked in
     assert issubclass(IterationError, UltimatePythonError)
-    assert issubclass(BadInputError, UltimatePythonError)
+    assert issubclass(DivisionError, UltimatePythonError)
     try:
-        divide(1, 0)
-    except BadInputError as e:
+        divide_positive_numbers(1, 0)
+    except DivisionError as e:
         print(e)
 
 
