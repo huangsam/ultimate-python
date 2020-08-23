161d5566a062a90a8ba7ec4a340ad5ac4575225c
Samuel Huang <samhuang91@gmail.com>
2020-08-22 21:00:41 -0700

    Add more explanations on exception_class

--- ultimatepython/classes/exception_class.py
+++ ultimatepython/classes/exception_class.py
@@ -8,18 +8,42 @@ class UltimatePythonError(Exception):
 
 
 class IterationError(UltimatePythonError, RuntimeError):
-    """Any error that comes while iterating through objects."""
+    """Any error that comes while iterating through objects.
+
+    This is used in `ultimatepython.classes.iterator_class` so please do
+    NOT delete this class.
+    """
 
 
 class DivisionError(UltimatePythonError, ValueError):
-    """Any division error that results from invalid input."""
+    """Any division error that results from invalid input.
+
+    This can be complemented with the following exceptions if they
+    happen enough across the codebase:
+
+    - ZeroDivisorError
+    - NegativeDividendError
+    - NegativeDivisorError
+
+    That being said, there's a point of diminishing returns when
+    we design too many exceptions. It's better to design a few
+    that most developers handle than design many that few
+    developers handle.
+    """
 
 
 def divide_positive_numbers(dividend, divisor):
-    """Divide a positive number by another positive number"""
+    """Divide a positive number by another positive number.
+
+    Writing a program in this style is what is considered defensive
+    programming. For more on this programming style, check out the
+    Wikipedia link below:
+
+    https://en.wikipedia.org/wiki/Defensive_programming
+    """
     if divisor == 0:
         raise DivisionError("Cannot have a zero divisor")
-    elif dividend < 0:
+    elif dividend <= 0:
         raise DivisionError("Cannot have a negative dividend")
     elif divisor < 0:
         raise DivisionError("Cannot have a negative divisor")
