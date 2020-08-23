69d34a5e86aed3e259a266408d63ba63528a7ad2
Samuel Huang <samhuang91@gmail.com>
2020-08-22 21:15:51 -0700

    Add package setup configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ultimatepython",
+    packages=find_packages(),
+    description="Ultimate Python study guide.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
