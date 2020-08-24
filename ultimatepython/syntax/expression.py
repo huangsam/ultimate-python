b7003fb1965c2d3a25220dddb449a16bfae86cee
Samuel Huang <samhuang91@gmail.com>
2020-08-23 18:40:22 -0700

    Add more print and assert in ultimatepython

--- ultimatepython/syntax/expression.py
+++ ultimatepython/syntax/expression.py
@@ -3,25 +3,25 @@ def main():
     x = 1
 
     # Its value can used as part of expressions
-    print(x + 1)
+    print("Add integer", x + 1)
 
     # An expression can be chained indefinitely. This concept of chaining
     # expressions is powerful because it allows you to compose simple pieces
     # of code into larger pieces of code over time
-    print(x * 2 * 2 * 2)
+    print("Multiply integers", x * 2 * 2 * 2)
 
     # Division is a bit tricky in Python because it returns a result
     # of type 'float' by default
-    print(x / 2)
+    print("Divide as float", x / 2)
 
     # If an integer division is desired, then an extra slash
     # must be added to the expression
-    print(x // 2)
+    print("Divide by integer", x // 2)
 
     # Powers of an integer can be leveraged too. If you want more math
     # features, then you will have to leverage the builtin `math` library,
     # a third-party library or your own library
-    print(x * 2 ** 3)
+    print("Power of integer", x * 2 ** 3)
 
 
 if __name__ == "__main__":
--- ultimatepython/syntax/variable.py
+++ ultimatepython/syntax/variable.py
@@ -6,24 +6,30 @@ def main():
     d = "hello"
 
     # Notice that each type is a `class`. Each of the variables above refers
-    # to an `instance` of the class it belongs to
+    # to an `instance` of the class it belongs to. For instance, `a` is of type
+    # `int` which is a `class`
     a_type = type(a)  # <class 'int'>
     b_type = type(b)  # <class 'float'>
     c_type = type(c)  # <class 'bool'>
     d_type = type(d)  # <class 'str'>
 
-    # This leads to an important point: everything is an object in Python
-    a_is_obj = isinstance(a, object) and issubclass(a_type, object)
-    b_is_obj = isinstance(b, object) and issubclass(b_type, object)
-    c_is_obj = isinstance(c, object) and issubclass(c_type, object)
-    d_is_obj = isinstance(d, object) and issubclass(d_type, object)
+    # This leads to an important point: everything is an object in Python.
+    # Notice that all instances and classes are objects. Also, say hello
+    # to the `assert` keyword! This is a debugging aid that we use to validate
+    # the code as we progress through the `main` functions. These statements
+    # are used to validate the correctness of the data and to reduce
+    # the amount of standard output that is sent when running `main`
+    assert isinstance(a, object) and isinstance(a_type, object)
+    assert isinstance(b, object) and isinstance(b_type, object)
+    assert isinstance(c, object) and isinstance(c_type, object)
+    assert isinstance(d, object) and isinstance(d_type, object)
 
     # Here is a summary via the `print` function. Notice that we print more
     # than one variable at a time
-    print(a, a_type, a_is_obj)
-    print(b, b_type, b_is_obj)
-    print(c, c_type, c_is_obj)
-    print(d, d_type, d_is_obj)
+    print(a, a_type)
+    print(b, b_type)
+    print(c, c_type)
+    print(d, d_type)
 
 
 if __name__ == "__main__":
