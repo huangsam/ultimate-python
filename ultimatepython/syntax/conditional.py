45ae838fd4641888341b1476b89754ac947e241e
Samuel Huang <samhuang91@gmail.com>
2020-08-23 18:45:11 -0700

    Fixup conditional lesson

--- ultimatepython/syntax/conditional.py
+++ ultimatepython/syntax/conditional.py
@@ -4,32 +4,32 @@ def main():
 
     # This condition is obviously true
     if x_add_two == 3:
-        print("math wins")  # run
+        print("Math wins")  # run
 
     # A negated condition can also be true
     if not x_add_two == 1:
-        print("math wins here too")  # run
+        print("Math wins here too")  # run
 
     # There are `else` statements as well, which run if the initial condition
     # fails. Notice that one line gets skipped, and that this conditional
     # does not help one make a conclusion on the variable's true value
     if x_add_two == 1:
-        print("math lost here...")  # skip
+        print("Math lost here...")  # skip
     else:
-        print("math wins otherwise")  # run
+        print("Math wins otherwise")  # run
 
     # The `else` statement also run once every other `if` and `elif` condition
     # fails. Notice that multiple lines get skipped, and that all of the
     # conditions could have been compressed to `x_add_two != 3`
     # for simplicity. In this case, less is more
     if x_add_two == 1:
-        print("nope not this one...")  # skip
+        print("Nope not this one...")  # skip
     elif x_add_two == 2:
-        print("nope not this one either...")  # skip
+        print("Nope not this one either...")  # skip
     elif x_add_two < 3 or x_add_two > 3:
-        print("nope not quite...")  # skip
+        print("Nope not quite...")  # skip
     else:
-        print("math wins finally")  # run
+        print("Math wins finally")  # run
 
 
 if __name__ == "__main__":
