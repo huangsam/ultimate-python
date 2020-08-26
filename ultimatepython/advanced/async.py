9dfe965b0ce6cb23fa83fe47bd179a2b031eacef
Samuel Huang <samhuang91@gmail.com>
2020-08-25 23:05:04 -0700

    Rename task to job for clarity

--- ultimatepython/advanced/async.py
+++ ultimatepython/advanced/async.py
@@ -5,8 +5,8 @@ from uuid import uuid4
 
 
 @dataclass
-class TaskRecord:
-    """Task record with useful metadata."""
+class JobRecord:
+    """Job record with useful metadata."""
 
     tid: int
     queued_at: datetime
@@ -18,34 +18,35 @@ def current_time():
     return datetime.now()
 
 
-async def start_task(delay, task_id):
+async def start_job(delay, job_id):
     """Start task_id after a certain delay in seconds."""
     queue_time = current_time()
-    print(f"{queue_time} -> Queue task {task_id[:16]}...")
+    print(f"{queue_time} -> Queue job {job_id[:16]}...")
     await asyncio.sleep(delay)
     start_time = current_time()
-    print(f"{start_time} -> Start task {task_id[:16]}...")
-    return TaskRecord(task_id, queue_time, start_time)
+    print(f"{start_time} -> Start job {job_id[:16]}...")
+    return JobRecord(job_id, queue_time, start_time)
 
 
 async def start_batch():
-    """Start a batch of tasks concurrently.
+    """Start a batch of jobs concurrently.
 
     Each item in the `tasks` list is a `Task` which is an instance of
     a `Future`. The `Task` instance was created by providing a coroutine
-    instance from `start_task` into the `create_task` function.
+    instance from `start_job` into the `create_task` function.
 
-    After awaiting the list of tasks, we get a list of `TaskRecord` items
+    After awaiting the list of tasks, we get a list of `JobRecord` items
     with characteristics that we expect.
     """
     print(f"{current_time()} -> Send kickoff email")
 
-    tasks = [asyncio.create_task(start_task(i * .01, uuid4().hex))
+    tasks = [asyncio.create_task(start_job(i * .01, uuid4().hex))
              for i in range(1, 5)]
 
     # Gather all tasks for batch start
-    task_records = await asyncio.gather(*tasks)
-    for record in task_records:
+    job_records = await asyncio.gather(*tasks)
+    for record in job_records:
+        assert isinstance(record, JobRecord)
         assert record.queued_at < record.started_at
 
     print(f"{current_time()} -> Send confirmation email")
