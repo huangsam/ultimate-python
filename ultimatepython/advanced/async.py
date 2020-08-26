e2d614a0ccefd11c66bcf8c44615acc9b363d091
Samuel Huang <samhuang91@gmail.com>
2020-08-25 22:29:28 -0700

    Refine async lesson again

--- ultimatepython/advanced/async.py
+++ ultimatepython/advanced/async.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 
 
 # Define a tuple with named records
-@dataclass(order=True)
+@dataclass
 class TaskRecord:
     tid: str
     queued_at: datetime
@@ -20,26 +20,26 @@ def current_time():
 async def start_task(delay, task_id):
     """Start task_id after a certain delay in seconds."""
     queue_time = current_time()
-    print(f"{queue_time} -> Queued task {task_id[:16]}...")
+    print(f"{queue_time} -> Queue task {task_id[:16]}...")
     await asyncio.sleep(delay)
     start_time = current_time()
-    print(f"{start_time} -> Started task {task_id[:16]}...")
+    print(f"{start_time} -> Start task {task_id[:16]}...")
     return TaskRecord(task_id, queue_time, start_time)
 
 
 async def start_batch():
-    """Create a batch of tasks them concurrently."""
-    print(f"{current_time()} -> Send initiation email")
+    """Start a batch of tasks concurrently."""
+    print(f"{current_time()} -> Send kickoff email")
 
     tasks = [asyncio.create_task(start_task(i * .01, uuid4().hex))
              for i in range(1, 5)]
 
-    # Gather all tasks for batch completion
+    # Gather all tasks for batch start
     task_records = await asyncio.gather(*tasks)
     for record in task_records:
         assert record.queued_at < record.started_at
 
-    print(f"{current_time()} -> Send completion email")
+    print(f"{current_time()} -> Send confirmation email")
 
 
 def main():
