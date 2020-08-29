2b98090215c8063667f71c343709b12722393310
Samuel Huang <samhuang91@gmail.com>
2020-08-28 18:30:21 -0700

    Minimize setup.py description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ultimatepython",
     packages=find_packages(),
-    description="Ultimate Python study guide for newcomers and professionals alike.",
+    description="Ultimate Python study guide",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
