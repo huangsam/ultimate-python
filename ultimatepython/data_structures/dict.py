e3d6a78148bb5f3a4964bc84b36657384fb93b73
Samuel Huang <samhuang91@gmail.com>
2020-09-06 15:44:01 -0700

    Improve content in dict lesson

--- ultimatepython/data_structures/dict.py
+++ ultimatepython/data_structures/dict.py
@@ -1,22 +1,28 @@
 def main():
-    # Each student has a name key and a GPA value
+    # Let's create a mapping of student name to GPA
     student_gpa = {"john": 3.5,
                    "jane": 4.0,
                    "bob": 2.8,
                    "mary": 3.2}
 
-    # You can access the value of a particular key
-    assert student_gpa["john"] == 3.5
+    # There are four students
+    assert len(student_gpa) == 4
 
-    # You can access the dictionary keys in isolation
+    # Each student has a name key and a GPA value
+    assert len(student_gpa.keys()) == len(student_gpa.values())
+
+    # You can get the names in isolation
     for student in student_gpa.keys():
         assert len(student) > 2
 
-    # You can access the dictionary values in isolation
+    # You can get the GPAs in isolation
     for gpa in student_gpa.values():
         assert gpa > 2.0
 
-    # You can access the dictionary keys and values simultaneously
+    # You can get the GPA for a specific student
+    assert student_gpa["john"] == 3.5
+
+    # You can access the student and GPA simultaneously
     for student, gpa in student_gpa.items():
         print(f"Student {student} has a {gpa} GPA")
 
