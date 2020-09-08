33c4b503879b880a00126fac21251117a32ae7e5
Samuel Huang <samhuang91@gmail.com>
2020-09-08 07:42:14 -0700

    Remove unused variables in conditional

--- ultimatepython/syntax/conditional.py
+++ ultimatepython/syntax/conditional.py
@@ -17,7 +17,6 @@ def main():
     # There are `else` statements as well, which run if the initial condition
     # fails. Notice that one line gets skipped, and that this conditional
     # does not help one make a conclusion on the variable's true value
-    ran_3 = False
     if x_add_two == 1:
         ran_3 = False  # skip: if
     else:
@@ -28,7 +27,6 @@ def main():
     # fail. Notice that multiple lines get skipped, and that all of the
     # conditions could have been compressed to `x_add_two != 3` for
     # simplicity. In this case, less logic results in more clarity
-    ran_4 = False
     if x_add_two == 1:
         ran_4 = False  # skip: if
     elif x_add_two == 2:
