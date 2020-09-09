dbdbe5a5983fb3eb271bf9a8b04c7887af1c7fff
Samuel Huang <samhuang91@gmail.com>
2020-09-09 09:16:39 -0700

    Change code to match wording in basic_class

--- ultimatepython/classes/basic_class.py
+++ ultimatepython/classes/basic_class.py
@@ -60,8 +60,8 @@ def main():
     assert driving.__self__ == car
 
     # That is why `driving` is a method and NOT a function
-    assert not isfunction(driving)
     assert ismethod(driving)
+    assert not isfunction(driving)
 
     # And there is only one parameter for `driving` because `__self__`
     # binding is implicit
