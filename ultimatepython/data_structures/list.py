63a4a21dddee84f8252fdfa25e683f0c7379386b
Samuel Huang <samhuang91@gmail.com>
2020-09-11 10:01:17 -0700

    Add assertions in list lesson

--- ultimatepython/data_structures/list.py
+++ ultimatepython/data_structures/list.py
@@ -60,8 +60,10 @@ def main():
 
     # Lists can be nested at arbitrary levels
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    assert matrix[1][0] == 4
+    assert matrix[0][0] == 1
     assert matrix[0][1] == 2
+    assert matrix[1][0] == 4
+    assert matrix[1][1] == 5
 
     # This matrix just so happens to be a square so that the length of each
     # row is the same as the number of rows in the matrix
