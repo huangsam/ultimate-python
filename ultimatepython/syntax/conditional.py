b102a8b6764d6f5956015988401c46156dff9aa8
Samuel Huang <samhuang91@gmail.com>
2020-09-11 08:38:38 -0700

    Update conditional.py

--- ultimatepython/syntax/conditional.py
+++ ultimatepython/syntax/conditional.py
@@ -23,7 +23,7 @@ def main():
         ran_3 = True  # run
     assert ran_3 is True
 
-    # The `else` statement also run once all other `if` and `elif` conditions
+    # The `else` statement also runs once all other `if` and `elif` conditions
     # fail. Notice that multiple lines get skipped, and that all of the
     # conditions could have been compressed to `x_add_two != 3` for
     # simplicity. In this case, less logic results in more clarity
