321081b37ca01be97bba77f0394a3c9dc4834118
Samuel Huang <samhuang91@gmail.com>
2020-10-11 13:55:02 -0700

    Improve mixins lesson

--- ultimatepython/advanced/mixins.py
+++ ultimatepython/advanced/mixins.py
@@ -30,7 +30,6 @@ class TemplateHandlerMixin(RequestHandler):
     def get_template_name(self, request_url):
         raise NotImplementedError
 
-    @abstractmethod
     def is_valid_template(self, template_name):
         return template_name.endswith(self.template_suffix)
 
@@ -41,13 +40,15 @@ class TemplateHandlerMixin(RequestHandler):
 
 class AuthHandlerMixin(RequestHandler):
     """Abstract auth mixin for RequestHandler."""
-    success_content = "<p>Something private</p>"
-    failure_content = "<p>Access denied</p>"
 
     def handle(self, request):
-        if self.is_valid_user(request.user):
-            return self.success_content
-        return self.failure_content
+        if not self.is_valid_user(request.user):
+            return self.get_failure_content(request)
+        return super().handle(request)
+
+    @staticmethod
+    def get_failure_content(request):
+        return f"<p>Access denied for {request.url}</p>"
 
     @abstractmethod
     def is_valid_user(self, request_user):
@@ -70,11 +71,11 @@ class TemplateFolderHandler(TemplateHandlerMixin):
         return self.template_dir[template_name]
 
 
-class AdminHandler(AuthHandlerMixin):
+class AdminHandler(AuthHandlerMixin, TemplateFolderHandler):
     """Concrete auth handler."""
-    success_content = "<p>Admin stuff</p>"
 
-    def __init__(self, admin_users):
+    def __init__(self, admin_users, template_dir):
+        super().__init__(template_dir)
         self.admin_users = admin_users
 
     def is_valid_user(self, request_user):
@@ -82,13 +83,28 @@ class AdminHandler(AuthHandlerMixin):
 
 
 def main():
-    welcome_handler = TemplateFolderHandler({"welcome.template": "<p>Hello world</p>"})
-    assert welcome_handler.handle(Request("/welcome.template", "nobody")) == "<p>Hello world</p>"
-    assert welcome_handler.handle(Request("/foo.bar", "nobody")) == "<p>Not found</p>"
+    # Attempt requests with unauthenticated template handler
+    simple_dir = {"welcome.template": "<p>Hello world</p>",
+                  "about.template": "<p>About me</p>"}
+    welcome_from_nobody = Request("/welcome.template", "nobody")
+    about_from_nobody = Request("/about.template", "nobody")
+    foo_from_nobody = Request("/foo.bar", "nobody")
+    simple_handler = TemplateFolderHandler(simple_dir)
+    assert simple_handler.handle(welcome_from_nobody) == "<p>Hello world</p>"
+    assert simple_handler.handle(about_from_nobody) == "<p>About me</p>"
+    assert simple_handler.handle(foo_from_nobody) == "<p>Not found</p>"
 
-    admin_handler = AdminHandler({"john", "jane"})
-    assert admin_handler.handle(Request("/admin.html", "john")) == "<p>Admin stuff</p>"
-    assert admin_handler.handle(Request("/admin.html", "nobody")) == "<p>Access denied</p>"
+    # Attempt requests with authenticated template handler
+    admin_users = {"john", "jane"}
+    admin_dir = {"fqdn.template": "<p>server.example.com</p>",
+                 "salary.template": "<p>123456789.00</p>"}
+    fqdn_from_john = Request("/fqdn.template", "john")
+    salary_from_jane = Request("/salary.template", "jane")
+    salary_from_nobody = Request("/salary.template", "nobody")
+    admin_handler = AdminHandler(admin_users, admin_dir)
+    assert admin_handler.handle(fqdn_from_john) == "<p>server.example.com</p>"
+    assert admin_handler.handle(salary_from_jane) == "<p>123456789.00</p>"
+    assert admin_handler.handle(salary_from_nobody) == "<p>Access denied for /salary.template</p>"
 
 
 if __name__ == '__main__':
