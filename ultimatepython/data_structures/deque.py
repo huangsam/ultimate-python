9ec934a926acbab0cb92c799811711a357484eda
Samuel Huang <samhuang91@gmail.com>
2020-10-19 23:05:01 -0700

    Refine comments in deque lesson

--- ultimatepython/data_structures/deque.py
+++ ultimatepython/data_structures/deque.py
@@ -3,14 +3,14 @@ from collections import deque
 
 def main():
     # A list is identical to a vector where a new array is created when
-    # there are too many elements and the old elements are moved over to
-    # the new array one-by-one. The time complexity involved with growing
-    # its size is linear. A deque is identical to a doubly linked list
-    # whose nodes have a left pointer and a right pointer. In order to
-    # grow the linked list, a new node is created and added to the left
-    # or the right of the linked list. The time complexity involved with
-    # growing its size is constant. Check out the source code for a list
-    # and a deque to learn more:
+    # there are too many elements in the old array, and the old array
+    # elements are moved over to the new array one-by-one. The time
+    # involved with growing its size increases linearly. A deque is
+    # identical to a doubly linked list whose nodes have a left pointer
+    # and a right pointer. In order to grow the linked list, a new node
+    # is created and added to the left or the right of the linked list.
+    # The time complexity involved with growing its size is constant.
+    # Check out the source code for a list and a deque here:
     # https://github.com/python/cpython/blob/3.8/Objects/listobject.c
     # https://github.com/python/cpython/blob/3.8/Modules/_collectionsmodule.c
     dq = deque()
@@ -22,9 +22,10 @@ def main():
         # Similar to adding a new node to the left of the linked list
         dq.appendleft(i * 2)
 
-    # A deque can be iterated on to build a sequence of choice
+    # A deque can be iterated over to build any data structure
     assert [el for el in dq] == [8, 6, 4, 2, 1, 2, 3, 4]
     assert tuple(el for el in dq) == (8, 6, 4, 2, 1, 2, 3, 4)
+    assert {el for el in dq} == {8, 6, 4, 2, 1, 3}
 
     # A deque can be used as a stack
     # https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
