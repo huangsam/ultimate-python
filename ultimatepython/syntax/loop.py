c462da619789266f5a53d690ea02bdce07776717
Samuel Huang <samhuang91@gmail.com>
2020-10-27 07:12:21 -0700

    Add module docs for syntax modules

--- ultimatepython/syntax/loop.py
+++ ultimatepython/syntax/loop.py
@@ -1,3 +1,12 @@
+"""
+Loops are to expressions as multiplication is to addition. They help us
+run the same code many times until a certain condition is met. This
+module shows how to use the for-loop and while-loop, while also showing
+how `continue` and `break` give us fine-grained control on a
+loop's lifecycle.
+"""
+
+
 def main():
     # This is a `for` loop that iterates on values 0..4 and adds each
     # value to `total`. It leverages the `range` iterator. Providing
