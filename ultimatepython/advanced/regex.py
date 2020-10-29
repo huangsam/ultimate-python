a264200570c6526995762c35270b6cfe968c7ea2
Samuel Huang <samhuang91@gmail.com>
2020-10-28 22:55:14 -0700

    Enhance docs of advanced modules

--- ultimatepython/advanced/regex.py
+++ ultimatepython/advanced/regex.py
@@ -1,8 +1,8 @@
 """
 Regular expressions is a way to search text efficiently. Implementing them
 is difficult but thankfully Python provides a package for us to use them
-easily. This module shows a few examples of use the `re` package to search
-predefined text snippets stored in module-level constants.
+easily. This module shows a few examples of how to use the `re` package to
+search predefined text snippets stored in module-level constants.
 """
 import re
 
