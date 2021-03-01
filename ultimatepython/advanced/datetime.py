a489a1c9c9e27c6fc3f600bbe2ec7dd0ae8a27bb
Samuel Huang <samhuang91@gmail.com>
2021-03-01 14:39:47 -0800

    Add clarification on datetime offsets

--- ultimatepython/advanced/datetime.py
+++ ultimatepython/advanced/datetime.py
@@ -35,6 +35,15 @@ def main():
     assert utc_dt.tzinfo is timezone.utc
     assert convert_dt_to_utc_epoch(utc_dt) == naive_dt_epoch
 
+    # Cannot compute difference between offset-naive and offset-aware
+    # datetime objects
+    calc_failed = False
+    try:
+        _ = utc_dt - naive_dt
+    except TypeError:
+        calc_failed = True
+    assert calc_failed is True
+
     # Create new UTC time as datetime
     utc_dt_new_one = get_utc_now_as_dt()
     assert utc_dt_new_one > utc_dt
