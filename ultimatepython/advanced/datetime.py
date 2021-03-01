e1ee39805a50bed9096ac74e7acabe32f7e099cc
Samuel Huang <samhuang91@gmail.com>
2021-03-01 14:32:52 -0800

    Implement datetime lesson

---
+++ ultimatepython/advanced/datetime.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timezone
+
+
+def convert_dt_to_utc_epoch(dt):
+    """Convert datetime to UTC time in epoch seconds."""
+    return dt.timestamp()
+
+
+def convert_utc_epoch_to_dt(epoch):
+    """Convert epoch seconds to datetime."""
+    return datetime.fromtimestamp(epoch, tz=timezone.utc)
+
+
+def get_utc_now_as_dt():
+    """Get current UTC time as datetime."""
+    return datetime.now(tz=timezone.utc)
+
+
+def get_utc_now_as_epoch():
+    """Get current UTC time as epoch seconds."""
+    return convert_dt_to_utc_epoch(get_utc_now_as_dt())
+
+
+def main():
+    # Create timezone-naive datetime
+    naive_dt = datetime.now()
+    assert naive_dt.tzinfo is None
+
+    # Change timezone-naive datetime to epoch seconds
+    naive_dt_epoch = convert_dt_to_utc_epoch(naive_dt)
+    assert naive_dt_epoch > 0
+
+    # Change epoch seconds to UTC datetime
+    utc_dt = convert_utc_epoch_to_dt(naive_dt_epoch)
+    assert utc_dt.tzinfo is timezone.utc
+    assert convert_dt_to_utc_epoch(utc_dt) == naive_dt_epoch
+
+    # Create new UTC time as datetime
+    utc_dt_new_one = get_utc_now_as_dt()
+    assert utc_dt_new_one > utc_dt
+
+    # Create another new UTC time as epoch seconds
+    utc_epoch_new_two = get_utc_now_as_epoch()
+    utc_epoch_new_one = convert_dt_to_utc_epoch(utc_dt_new_one)
+
+    # Epoch seconds line up as expected
+    assert utc_epoch_new_two > utc_epoch_new_one > naive_dt_epoch
+
+
+if __name__ == "__main__":
+    main()
