c10a32324284486ffc1fc208398254939b983f21
Farhan Hai Khan <njrfarhandasilva10@gmail.com>
2021-10-02 03:16:56 +0530

    Add Email Regex for Example Purposes #44

--- ultimatepython/advanced/regex.py
+++ ultimatepython/advanced/regex.py
@@ -3,19 +3,31 @@ Using regular expressions is a robust way to search for text. Implementing
 them is difficult but Python provides a package for us to use them easily.
 This module shows a few examples of how to use the `re` package to search
 predefined text snippets stored in module-level constants.
+
+Example : Added Email Search Regex
 """
+
 import re
+
+def EmailExtract(txt):
+    ValidatedAtDotEmailRegex = r"[a-z0-9\.\-+_]+(?:@|\[at\])[a-z0-9\.\-+_]+(?:\.|\[dot\])[a-z]+"
+    emailMatch = re.findall(ValidatedAtDotEmailRegex, txt)
+    return emailMatch
 
 # Module-level constants
 _TEXT_HELLO = "World Hello Hello"
 _TEXT_NAMES = "John, Jane"
 _TEXT_ABC123 = "abc123"
 _TEXT_BYE = "Bye for now"
+_TEXT_LONG_EMAIL_ = "This is a sample email that should get noticed in the regex. My email is njrfarhandasilva10@gmail.com and I am an Open Source Enthusiast."
 
 
 def main():
     # Running `search` with "Hello" has a match for first Hello
     assert re.search(r"Hello", _TEXT_HELLO).start() == 6
+    
+    # Running Email Check on Text
+    assert EmailExtract(_TEXT_LONG_EMAIL_) == ["njrfarhandasilva10@gmail.com"]
 
     # Running `search` with "Hello$" has a match for last Hello
     assert re.search(r"Hello$", _TEXT_HELLO).start() == 12
