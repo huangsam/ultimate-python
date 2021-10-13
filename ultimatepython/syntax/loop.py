a22763e9fce85f60c556d7a21f28b7ca6a1a9e97
Samuel Huang <samhuang91@gmail.com>
2021-10-13 09:52:30 -0700

    Update loop.py

--- ultimatepython/syntax/loop.py
+++ ultimatepython/syntax/loop.py
@@ -3,6 +3,11 @@ Loops are to expressions as multiplication is to addition. They help us
 run the same code many times until a certain condition is not met. This
 module shows how to use the for-loop and while-loop, and also shows how
 `continue` and `break` give us precise control over a loop's lifecycle.
+
+Note that for-else and while-else loops exist in Python, but they are
+not commonly used. Please visit this link for some examples:
+
+https://stackoverflow.com/a/59491247/9921431
 """
 
 
@@ -77,9 +82,6 @@ def main():
 
     # The `else` clause can be added after `while`/`for` loop.
     # It is executed should the loop-condition becomes false.
-    # If there is a `break` in the loop, the `else` clause
-    # will not be executed if `break` terminates the loop.
-    # Example: https://stackoverflow.com/a/59491247/9921431
 
 
 if __name__ == "__main__":
