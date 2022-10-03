800a44d6a3a3983416bc796b80bbeb881d29b392
Khizar Abbasi <93927942+khizzer2323@users.noreply.github.com>
2022-10-03 21:48:10 +0500

    added this

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
