ad0f25549329e7499482948a76c3d12edf377650
Samuel Huang <samhuang91@gmail.com>
2022-10-18 08:46:11 -0700

    Update conditional.py
    Making some small tweaks for #60. Thanks again @j3r3mias 😄

--- ultimatepython/syntax/conditional.py
+++ ultimatepython/syntax/conditional.py
@@ -43,9 +43,9 @@ def main():
         ran_4 = True  # run
     assert ran_4 is True
 
-    # Conditionals can also be written in one line using `if` and `else`, in the
-    # following form: A if condition else B
-    # This can be used in assignments, for example.
+    # Conditionals can also be written in one line using `if` and `else`
+    # with the following form: A if condition else B. This can be used
+    # for assignments as shown below
     ran_5 = False
     ran_5 = True if x_add_two == 3 else False
     assert ran_5 is True
