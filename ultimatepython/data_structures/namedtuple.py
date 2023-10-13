c328abb8d69818f923f91f06886d68926125bca1
Ajay Gonepuri (GONAPCORP) <98868227+HKABIG@users.noreply.github.com>
2023-10-13 08:49:03 +0530

    Update namedtuple.py
    added asserts and main function and blank lines to pass circle CI test

--- ultimatepython/data_structures/namedtuple.py
+++ ultimatepython/data_structures/namedtuple.py
@@ -7,38 +7,39 @@ tuples are created using the namedtuple function from the collections module.
 from collections import namedtuple
 
 def main():
+    # Named Tuple Attributes:
+    # - namedtuple: A function from the collections module used to define a named tuple type.
+    # - Point: A named tuple type with fields 'x' and 'y.
     Point = namedtuple('Point', ['x', 'y'])
 
-    # Create instances of the named tuple
+    # Named Tuple Fields:
+    # - x and y: Fields of the named tuple Point representing coordinates.
+    # - point1 and point2: Instances of the Point named tuple.
     point1 = Point(x=1, y=2)
     point2 = Point(x=3, y=4)
 
-    # Accessing fields using dot notation
+    # Named Tuple Operations:
+    # - Accessing fields using dot notation.
+    # - Named tuples are immutable.
+    # - Named tuples support tuple operations.
+    # - Converting named tuples to dictionaries and vice versa.
+    # - Additional methods and attributes.
     assert point1.x == 1
     assert point1.y == 2
     assert point2.x == 3
     assert point2.y == 4
-
-    # Named tuples are immutable
     try:
         point1.x = 5  # Attempt to change the 'x' field of point1 (raises an error)
     except AttributeError:
         pass
-
-    # Named tuples support tuple operations
-    assert point1[0] + point2[0] == 4  # Access elements like a tuple and perform addition
-
-    # Converting named tuples to dictionaries and vice versa
-    point_dict = point1._asdict()  # Convert point1 to a dictionary
+    assert point1[0] + point2[0] == 4
+    point_dict = point1._asdict()
     assert point_dict == {'x': 1, 'y': 2}
-
-    point3 = Point._make({'x': 10, 'y': 20})  # Create a new Point from a dictionary
+    point3 = Point._make({'x': 10, 'y': 20})
     assert point3.x == 10
     assert point3.y == 20
-
-    # Additional methods and attributes
-    assert Point._fields == ('x', 'y')  # Access field names as a tuple
-    point4 = point1._replace(x=5)  # Create a new Point with a field value changed
+    assert Point._fields == ('x', 'y')
+    point4 = point1._replace(x=5)
     assert point4.x == 5
     assert point4.y == 2
 
