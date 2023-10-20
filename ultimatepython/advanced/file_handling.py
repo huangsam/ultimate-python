7cae67b91c73ae13354aba5e80870ad451c2bc90
Archisman Bera <95976290+ArcXeon@users.noreply.github.com>
2023-10-20 23:14:29 +0530

    Update file_handling.py

--- ultimatepython/advanced/file_handling.py
+++ ultimatepython/advanced/file_handling.py
@@ -4,7 +4,9 @@ This code example demonstrates the basics of file handling in Python.
 Python provides various ways to work with files.
 We can use the built-in 'open' function to open files in different modes like reading ('r'), writing ('w'), and appending ('a').
 """
+import os
 
+_TARGET_FILE = "sample.txt"
 
 # Open a file for reading
 def read_file(filename):
@@ -27,22 +29,32 @@ def append_file(filename, content):
     return f"Content appended to '{filename}'."
 
 
+def delete_file(filename):
+    try:
+        os.remove(filename)
+        return f"'{filename}' has been deleted."
+
+
 def main():
     # Example of writing to a file
-    result = write_file("sample.txt", "This is a test.")
-    assert result == "Content written to 'sample.txt'."
+    result = write_file(_TARGET_FILE, "This is a test.")
+    assert result == f"Content written to '{_TARGET_FILE}'."
 
     # Example of reading a file
-    content = read_file("sample.txt")
+    content = read_file(_TARGET_FILE)
     assert content == "This is a test."
 
     # Example of appending to a file
-    append_result = append_file("sample.txt", "\nThis is an appended line.")
-    assert append_result == "Content appended to 'sample.txt'."
+    append_result = append_file(_TARGET_FILE, "\nThis is an appended line.")
+    assert append_result == f"Content appended to '{_TARGET_FILE}'."
 
     # Verify the content after appending
-    content = read_file("sample.txt")
+    content = read_file(_TARGET_FILE)
     assert content == "This is a test.\nThis is an appended line."
+
+    # Deleting the file
+    delete_result = delete_file(_TARGET_FILE)
+    assert delete_result == f"'{_TARGET_FILE}' has been deleted."
 
 
 if __name__ == "__main__":
