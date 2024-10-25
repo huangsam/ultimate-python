c2a6ca1c09fd7c2154f8a2506f0fd9c902877fb4
Samuel Huang <samhuang91@gmail.com>
2024-10-25 05:29:26 -0700

    Fixup variable naming

--- ultimatepython/syntax/variable.py
+++ ultimatepython/syntax/variable.py
@@ -46,7 +46,7 @@ def main():
     # We can use underscores (literal `_`) to separate digit groups in
     # integer literals
     assert 10_000 == 10000
-    assert 0x01_0f_2c == 69_420
+    assert 0x01_0F_2C == 69_420
     assert 3.456_290e-1 == 0.3_456_290
 
     # There is also a special literal called None. This literal is used to
