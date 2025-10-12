6c8dd5021e2574e21171d9399f39705158a123cf
Samuel Huang <samhuang91@gmail.com>
2025-10-11 20:06:28 -0700

    Update function.py

--- ultimatepython/syntax/function.py
+++ ultimatepython/syntax/function.py
@@ -5,21 +5,6 @@ a simple function and a composite function that uses the simple function
 in an interesting way.
 """
 
-def without_parameters():
-    """A function that does not accept parameters and does not return a value.
-
-    This function is not used in this module, but it is defined to illustrate
-    that functions do not need to accept parameters or return values.
-    """
-    print("This function does not accept parameters or return a value.")
-def sum(x: int, y: int) -> int:
-    """A function that accepts parameters and has type hints.
-
-    This function is not used in this module, but it is defined to illustrate
-    that functions can accept parameters and have type hints.
-    """
-    
-    return x + y
 
 def add(x, y):
     """Add two objects together to produce a new object.
@@ -48,6 +33,24 @@ def sum_until(fn, n):
     return total
 
 
+def without_parameters():
+    """A function that does not accept parameters and does not return a value.
+
+    This function is not used in this module, but it is defined to illustrate
+    that functions do not need to accept parameters or return values.
+    """
+    print("This function does not accept parameters or return a value.")
+
+
+def sum(x: int, y: int) -> int:
+    """A function that accepts parameters and has type hints.
+
+    This function is not used in this module, but it is defined to illustrate
+    that functions can accept parameters and have type hints.
+    """
+    
+    return x + y
+
 
 def main():
     # The `add` function can be used for numbers as expected
@@ -71,7 +74,6 @@ def main():
     assert "includes this docstring!" in sum_until.__doc__
 
     # Call the other two functions to ensure they work as expected
-    
     assert without_parameters() is None
     assert sum(1, 2) == 3
 
